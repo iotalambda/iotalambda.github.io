9ea0ce95a3ac559e96a05072814f59794b4a4d81
Joona Luoma <joona.luoma@hotmail.com>
2023-11-03 00:50:18 +0200

    Tweak layout

--- app/[...id]/page.tsx
+++ app/[...id]/page.tsx
@@ -19,24 +19,26 @@ export default async function Post(props: PostProps) {
   const [year, month, day, slug] = props.params.id
   const { html, title, prenote, date } = await getPostById({ year, month, day, slug })
   return (
-    <div className="md:px-10 mb-20">
-      <article>
-        <header className="px-5 mb-4 py-10 relative">
-          <h1 className="text-3xl leading-none tracking-tight text-gray-900 md:text-4xl lg:text-5xl dark:text-gray-100">
-            {title}
-          </h1>
-          <WrittenTime date={date} className="absolute" />
-        </header>
-        <div className="bg-slate-100 dark:bg-slate-800 p-5 rounded shadow-lg">
-          {prenote && (
-            <>
-              <p className="italic text-slate-400 p-4" dangerouslySetInnerHTML={{ __html: prenote }} />
-              <hr className="h-px my-4 bg-gray-200 border-0 dark:bg-gray-700" />
-            </>
-          )}
-          <div className="space-y-4" dangerouslySetInnerHTML={{ __html: html }} />
-        </div>
-      </article>
+    <div className="flex justify-center">
+      <div className="md:px-10 max-w-7xl mb-12">
+        <article>
+          <header className="px-5 mb-4 py-10 relative">
+            <h1 className="text-3xl leading-none tracking-tight text-gray-900 md:text-4xl lg:text-5xl dark:text-gray-100">
+              {title}
+            </h1>
+            <WrittenTime date={date} className="absolute" />
+          </header>
+          <div className="bg-slate-100 dark:bg-slate-800 p-5 pb-12 rounded shadow-lg">
+            {prenote && (
+              <>
+                <p className="italic text-slate-400 p-4" dangerouslySetInnerHTML={{ __html: prenote }} />
+                <hr className="h-px my-4 bg-gray-200 border-0 dark:bg-gray-700" />
+              </>
+            )}
+            <div className="space-y-4" dangerouslySetInnerHTML={{ __html: html }} />
+          </div>
+        </article>
+      </div>
     </div>
   )
 }
